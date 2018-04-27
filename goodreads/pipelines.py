2b8f821fc5597a661db670b96d6ae76aef8af791
Jana B <bejana@students.uni-mainz.de>
2018-04-27 11:01:25 +0200

    added papers for reference (by zahra), crawler now dl in json format

--- goodreads/pipelines.py
+++ goodreads/pipelines.py
@@ -5,28 +5,18 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from scrapy import signals
-from scrapy.contrib.exporter import CsvItemExporter
+import json
 from .spiders.GoodreadsSpider import GoodreadsSpider
 
 class GoodreadsPipeline(object):
-    exporter = None
-    @classmethod
-    def from_crawler(cls, crawler):
-        pipeline = cls()
-        crawler.signals.connect(pipeline.spider_opened, signals.spider_opened)
-        crawler.signals.connect(pipeline.spider_closed, signals.spider_closed)
-        return pipeline
+    def open_spider(self, spider):
+        filename = 'g_'+str(GoodreadsSpider.user_id)+'_'+str(GoodreadsSpider.pages[0])+'_'+str(GoodreadsSpider.pages[1])+'.jl'
+        self.file = open(filename, 'w')
 
-    def spider_opened(self, spider):
-        filename = 'g_'+str(GoodreadsSpider.user_id)+'_'+str(GoodreadsSpider.pages[0])+'_'+str(GoodreadsSpider.pages[1])+'.csv'
-        self.file = open(filename, 'w+b')
-        self.exporter = CsvItemExporter(self.file)
-        self.exporter.start_exporting()
-
-    def spider_closed(self, spider):
-        self.exporter.finish_exporting()
+    def close_spider(self, spider):
         self.file.close()
 
     def process_item(self, item, spider):
-        self.exporter.export_item(item)
+        line = json.dumps(dict(item)) + "\n"
+        self.file.write(line)
         return item
